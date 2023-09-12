87a9ca7cb587dbbe677671378d83dddd8abfb41e
gulamsaruar <saruar@mars.com.bd>
2023-09-12 17:14:03 +0600

    initial

---
+++ module-1/personal_info.php
@@ -0,0 +1,16 @@
+<?php
+    //Task 1: Personal Information Page
+
+    // Define variables
+    $name = "Md Gulam Saruar";
+    $age = 40; // your age
+    $country = "Bangladesh";
+    $introduction = "Hello, I am $name. I'm $age years old and I Live in $country. Thanks everyone !";
+
+    // personal information
+    echo trim('') ;
+    echo "Personal Information:\n";
+    echo "Name: $name \n";
+    echo "Age: $age \n";
+    echo "Country: $country \n";
+    echo "Introduction: $introduction";
---
+++ module-1/temperature_converter.php
@@ -0,0 +1,11 @@
+<?php
+
+//Task 2: Temperature Converter
+
+
+$far = 40;
+
+$cel = ($far - 32) * (5 / 9);
+
+echo "Temprature in Fahrenheit is : $far \n";
+echo "Temprature in Celcius is : $cel";
