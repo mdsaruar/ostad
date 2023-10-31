92a777a1b117c45e0c61be4cd75cd3d181a52f13
gulamsaruar <saruar@mars.com.bd>
2023-10-31 17:10:56 +0600

    new folder added

---
+++ module5/assignment-5/index.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+if (!isset($_SESSION["emailAddress"])) {
+    header("Location: login.php");
+}
+
+if ($_SESSION["role"] == "user") {
+    header("Location: home_user.php");
+} elseif ($_SESSION["role"] == "manager") {
+    header("Location: home_manager.php");
+} elseif ($_SESSION["role"] == "admin") {
+    header("Location: roleManagement.php");
+}
