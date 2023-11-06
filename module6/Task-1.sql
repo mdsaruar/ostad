7b2db7bff7ac2bfd4c0ee3a9bae431401e3f574b
gulamsaruar <saruar@mars.com.bd>
2023-11-06 17:03:35 +0600

    module 6

---
+++ module6/Task-1.sql
@@ -0,0 +1,21 @@
+
+/*
+	Task 1: 
+	Write a SQL query to retrieve all the customer information along with the total number of orders placed by each customer. Display the result in descending order of the number of orders.
+*/
+
+SELECT 
+    COUNT(`orders`.id) AS TotalOrders, 
+    customers.name AS Name, 
+    customers.email AS Email, 
+    customers.phone AS Phone,
+FROM 
+    `orders`
+LEFT JOIN 
+    customers 
+ON 
+    `orders`.customer_id = customers.id 
+GROUP BY 
+    customers.id
+ORDER BY 
+    TotalOrders DESC;
