a9fc252892b51271f03c4b8d1a512a82f18a2a07
gulamsaruar <saruar@mars.com.bd>
2023-11-19 12:10:44 +0600

    css changedassin

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+
+    public function index($id)
+    {
+
+        $name = "Donald Trump";
+        $age = "75";
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age
+        ];
+
+
+        $name = "access_token";
+        $value = "123-XYZ";
+        $minutes = 1;
+        $path = "/";
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+        $setCookie = cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return response($data, 200)->cookie($setCookie);
+    }
+}
